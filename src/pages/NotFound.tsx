390ad5dce5c64a024a64badac500494f273a3fb5
Amber Khan <42220923+InsideAmber@users.noreply.github.com>
2025-07-18 23:30:11 +0530

    refactored not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+import { ROUTES } from "../constants/routes";
+
 const NotFound = () => {
   return (
-    <div className="h-screen flex items-center justify-center text-red-600 text-xl font-bold">
-      🚫 404 - Page Not Found
+    <div className="h-screen flex flex-col items-center justify-center text-center text-red-600">
+      <div className="text-2xl font-bold mb-4">🚫 404 - Page Not Found</div>
+      <Link
+        to={ROUTES.HOME}
+        className="text-blue-500 underline hover:text-blue-700 transition"
+      >
+        ← Go back to Home
+      </Link>
     </div>
   );
 };
